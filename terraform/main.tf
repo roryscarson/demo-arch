2e61c412c2bc8a21ab6f7129036aa48487c82a8c
Rory Carson <roryscarson@gmail.com>
2025-03-15 18:17:52 +0100

    setup public routing

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+resource "aws_lb" "main_lb" {
+  name               = "demo-arch-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups = [
+    aws_security_group.internet_to_alb.id,
+    aws_security_group.internet_to_k8s.id,
+    aws_security_group.public_to_private.id
+  ]
+  subnets = [
+    aws_subnet.public1-euw1a.id,
+    aws_subnet.public2-euw1b.id,
+    aws_subnet.public3-euw1c.id
+  ]
+
+  //  enable_deletion_protection = true
+
+  //  access_logs {
+  //   bucket  = aws_s3_bucket.lb_logs.id
+  //   prefix  = "demo_arch_lb"
+  //   enabled = true
+  //  }
+}
---
+++ terraform/security_groups.tf
@@ -0,0 +1,110 @@
+import {
+  to = aws_security_group.internet_to_alb
+  id = "sg-00e2ccc4ae6bfddfe"
+}
+
+//Security group - A
+resource "aws_security_group" "internet_to_alb" {
+  name        = "internet_to_alb"
+  description = "Expose ALB to the internet"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "internet_to_alb"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_alb" {
+  security_group_id = aws_security_group.internet_to_alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+resource "aws_vpc_security_group_egress_rule" "allow_tls_alb" {
+  security_group_id = aws_security_group.internet_to_alb.id
+  cidr_ipv4         = "10.0.0.0/24" //public subnet all AZs
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+//Security group - B
+resource "aws_security_group" "internet_to_k8s" {
+  name        = "internet_to_k8s"
+  description = "Expose K8s to the internet"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "internet_to_k8s"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_k8s" {
+  security_group_id = aws_security_group.internet_to_k8s.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+resource "aws_vpc_security_group_egress_rule" "allow_tls_k8s" {
+  security_group_id = aws_security_group.internet_to_k8s.id
+  cidr_ipv4         = "10.0.0.0/24" //public subnet all AZs"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_http_k8s" {
+  security_group_id = aws_security_group.internet_to_k8s.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_egress_rule" "allow_http_k8s" {
+  security_group_id = aws_security_group.internet_to_k8s.id
+  cidr_ipv4         = "10.0.0.0/24" //public subnet all AZs"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+//Security group - C
+resource "aws_security_group" "public_to_private" {
+  name        = "public_to_private"
+  description = "Allow access to private subnet from public ones"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "public_to_private"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "private_rds" {
+  security_group_id = aws_security_group.public_to_private.id
+  cidr_ipv4         = "10.0.0.0/24"
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
+resource "aws_vpc_security_group_egress_rule" "private_rds" {
+  security_group_id = aws_security_group.public_to_private.id
+  cidr_ipv4         = aws_subnet.private1-euw1a.cidr_block
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
+resource "aws_vpc_security_group_ingress_rule" "private_http" {
+  security_group_id = aws_security_group.public_to_private.id
+  cidr_ipv4         = "10.0.0.0/24"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+resource "aws_vpc_security_group_egress_rule" "private_http" {
+  security_group_id = aws_security_group.public_to_private.id
+  cidr_ipv4         = aws_subnet.private1-euw1a.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
